2048bc474fbcb9f749c50deb1e910b9ddcc71852
wasifmislam <67533737+wasifmislam@users.noreply.github.com>
2020-10-01 14:29:27 +0600

    authentication done again

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcN3vFJ4-kCxaX5zAgnA2GV7j9_RYP8cA",
+    authDomain: "ema-john-simple76.firebaseapp.com",
+    databaseURL: "https://ema-john-simple76.firebaseio.com",
+    projectId: "ema-john-simple76",
+    storageBucket: "ema-john-simple76.appspot.com",
+    messagingSenderId: "607690840173",
+    appId: "1:607690840173:web:85fc6aae4e71f410cdc764"
+  };
+
+  export default firebaseConfig;
